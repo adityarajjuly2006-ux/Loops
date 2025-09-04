2046ce7997c1a78b667252b055ddf1bf03b0b78e
adityarajjuly2006-ux <adityarajjuly2006@gmail.com>
2025-09-04 15:44:16 +0530

    for loop

---
+++ c programs/for.c
@@ -0,0 +1,8 @@
+//print hello world
+#include<stdio.h>
+int main(){
+    for(int i=1;i<=10;i++)
+    printf("%d\n",i*24);
+    return 0;
+}
+
---
+++ c programs/while.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    int i;
+    i=10;
+    while(i>=1){
+        printf("%d\n",i*37);
+        i--;
+    }
+}
